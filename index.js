acfc62e6c6a9870f034ff21cd7b1b87d86964f56
aditya11011 <96792983+aditya11011@users.noreply.github.com>
2024-02-17 22:05:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+ alert("WARNING:   today is your  Happy Birthday");
+
+function clic(){
+    let x=document.getElementById("game");
+    x.style.opacity=0.6;
+  let x1=prompt("enter one of your friend's name");
+
+   document.getElementById("one").innerHTML=x1;
+     let x2=prompt("one more");
+   document.getElementById("two").innerHTML=x2;
+  let x3=prompt("one last");
+  document.getElementById("three").innerHTML=x3;
+  let x4=prompt("final last");
+  document.getElementById("four").innerHTML=x4;
+}
